3b960245f74664d0e6e0bb2a0cb2614a7416a407
Angela <lika.ponomarenko@mail.ru>
2023-10-25 13:28:56 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let a = 4
+let b = 55
+
+// console.log( a + b )
+// console.log( a * b )
+// console.log( a / b )
+// console.log( a % b )
+
+let c ='hello'
+let d = ' Angela'
+
+// console.log( c + d )
+// console.log( c - d )
+
+let e= 5
+let f= '5'
+
+// console.log( +f + e )
+
+let g = 2
+let h = undefined
